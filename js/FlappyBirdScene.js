e968b391cf066161e1b4a864a6021fb605fa4dab
Ali-Topal <zmac634@live.rhul.ac.uk>
2024-11-01 04:48:44 +0000

    Changed active users position

--- js/FlappyBirdScene.js
+++ js/FlappyBirdScene.js
@@ -78,7 +78,7 @@ class FlappyBirdScene extends Phaser.Scene {
 		}, 50);
 	
 		// Create a text object to display the active user count
-		this.activeUsersText = this.add.text(155, 0, 'Active Players: 0', {
+		this.activeUsersText = this.add.text(150, 0, 'Active Players: 0', {
 			fontFamily: 'font1',
 			fontSize: '24px', 
 			fill: '#ffffff',
